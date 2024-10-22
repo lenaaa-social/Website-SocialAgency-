8ba613e46cc65d590a42c7ef02f7244e1c8689b2
Nicoalex <46748047+Nicoalex@users.noreply.github.com>
2024-10-22 18:00:04 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        //Example for new color
+        midnight: '#121063',
+      }
+    },
+
+  },
+  plugins: [],
+}
+
