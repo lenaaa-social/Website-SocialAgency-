8970075877cf7a010f305bd20338884926d7cbd9
Nicoalex <46748047+Nicoalex@users.noreply.github.com>
2024-12-30 02:07:48 +0100

    Add alt text to all images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
     extend: {
       colors: {
         //Example for new color
-        backBeige: '#F5F5EF',
-        textPrimColor: '#C2B19C',
+        //backBeige: '#F5F5EF',
+        //textPrimColor: '#C2B19C',
+        backBeige: '#F7F7F2',
+        textPrimColor:'#B49D84'
       },
       fontFamily:{
         'ephesis' : ['Ephesis'],
